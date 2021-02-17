13e665bad875063dd978d36a1e0ab07d330eed7f
Jenni Davis <jrosedavis@outlook.com>
2021-02-17 09:29:54 -0700

    final updates

--- EmployeeSQL/HW9-1.11_Queries.sql
+++ EmployeeSQL/HW9-1.11_Queries.sql
@@ -1,8 +1,5 @@
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 --Per ERD, access Employee and Salary tables.
---Use select * from to call on Employees table
-select * from "Employees"
-
 --Use join to find salary
 
 select "Employees".emp_no, 
@@ -16,6 +13,7 @@ on "Employees".emp_no = "Salaries".emp_no;
 
 --2. List first name, last name, and hire date for employees who were hired in 1986.
 --Per ERD, access "Employees" table 
+
 select "Employees".first_name,
 "Employees".last_name,
 "Employees".hire_date
@@ -27,11 +25,11 @@ where date_part ('year', hire_date) = 1986;
 --Per ERD, access "Employees", "Departments", and "Department_Manager" tables
 --Use join 
 
-select "Department_Manager".dept_no, 
+select "Department_Manager".dept_no,
+"Departments".dept_name,
 "Department_Manager".emp_no,
 "Employees".last_name,
-"Employees".first_name,
-"Departments".dept_name
+"Employees".first_name
 from "Department_Manager"
 join "Departments"
 on "Department_Manager".dept_no = "Departments".dept_no
@@ -44,9 +42,9 @@ order by emp_no;
 --Per ERD, access "Employees", "Department_Employees", "Departments" tables
 
 select "Department_Employees".emp_no,
-"Departments".dept_name,
 "Employees".last_name,
-"Employees".first_name
+"Employees".first_name,
+"Departments".dept_name
 from "Department_Employees"
 join "Departments"
 on "Department_Employees".dept_no = "Departments".dept_no
@@ -57,9 +55,9 @@ on "Department_Employees".emp_no = "Employees".emp_no;
 --"Hercules" and last names begin with "B."
 --Per ERD, access "Employees" table
 
-select "Employees".sex,
-"Employees".first_name,
-"Employees".last_name
+select "Employees".first_name,
+"Employees".last_name,
+"Employees".sex
 from "Employees"
 where last_name LIKE 'B%'
 and first_name = 'Hercules';
@@ -67,9 +65,10 @@ and first_name = 'Hercules';
 --6. List all employees in the Sales department, including their employee number, 
 --last name, first name, and department name.
 --Per ERD, access "Departments", "Employees", and "Department_Employees"
+
 select "Department_Employees".emp_no,
-"Employees".first_name,
 "Employees".last_name,
+"Employees".first_name,
 "Departments".dept_name
 from "Department_Employees"
 join "Departments"
@@ -81,9 +80,10 @@ where dept_name = 'Sales';
 --7.List all employees in the Sales and Development departments, 
 --including their employee number, last name, first name, and department name.
 --PER ERD access "Departments", "Employees", and "Department_Employees"
+
 select "Department_Employees".emp_no,
-"Employees".first_name,
 "Employees".last_name,
+"Employees".first_name,
 "Departments".dept_name
 from "Department_Employees"
 join "Departments"
@@ -95,12 +95,8 @@ where dept_name in ('Sales', 'Development');
 --8. In descending order, list the frequency count of employee last names, i.e., 
 --how many employees share each last name.
 --Per ERD access "Employees"
+
 select last_name, count(*) as freq_count
 from "Employees"
 group by last_name
 order by freq_count desc;
-
-
-
-
-
